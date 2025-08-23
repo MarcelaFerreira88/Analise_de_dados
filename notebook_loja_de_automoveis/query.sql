1d0deaeddad3eb94d80ef3ad7642fbf67f046158
Marcela Ferreira <57438428+MarcelaFerreira88@users.noreply.github.com>
2025-08-23 10:08:44 -0300

    Update query.sql

--- notebook_loja_de_automoveis/query.sql
+++ notebook_loja_de_automoveis/query.sql
@@ -23,3 +23,55 @@ from sales.customers
 
 select *
 from sales.customers
+
+-- Seleção de uma coluna sem DISTINCT
+-- Liste as marcas de carro que constam na tabela products
+
+-- COMMAND ----------
+select brand
+from sales.products
+
+-- Seleção de uma coluna com DISTINCT
+-- Liste as marcas de carro distintas que constam na tabela products
+
+-- COMMAND ----------
+select distinct brand 
+from sales.products
+
+-- Seleção de mais de uma coluna com DISTINCT
+-- Liste as marcas e anos de modelo distintos que constam na tabela products
+
+-- COMMAND ----------
+select distinct brand, model_year
+from sales.products
+
+
+-- Filtro com condição única
+-- Liste os emails dos clientes da nossa base que moram no estado de Santa Catarina
+
+-- COMMAND ----------
+select email, state
+from sales.customers
+where state = 'SC'
+
+-- Filtro com mais de uma condição
+-- Liste os emails dos clientes da nossa base que moram no estado de Santa Catarina
+-- ou Mato Grosso do Sul
+
+-- COMMAND ----------
+select email, state
+from sales.customers
+where state = 'SC' or state = 'MS'
+
+
+-- Filtro de condição com data
+-- Liste os emails dos clientes da nossa base que moram no estado de Santa Catarina 
+-- ou Mato Grosso do Sul e que tem mais de 30 anos
+
+-- COMMAND ----------
+select email, state, birth_date
+from sales.customers
+where state = 'SC' or state = 'MS' and birth_date < '19911228'
+
+
+
