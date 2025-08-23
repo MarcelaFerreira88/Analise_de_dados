f58da272d161718fe5b821030d7eb936f10d42ef
Marcela Ferreira <57438428+MarcelaFerreira88@users.noreply.github.com>
2025-08-23 10:44:51 -0300

    Update query.sql

--- notebook_loja_de_automoveis/query.sql
+++ notebook_loja_de_automoveis/query.sql
@@ -3,15 +3,11 @@
 -- Seleção de uma coluna de uma tabela 
 -- Liste os e-mails dos clientes da tabela sales.customers
 
--- COMMAND ----------
-
 select email 
 from sales.customers
 
 -- Seleção de mais de uma coluna de uma tabela 
 -- Liste os e-mails e nomes dos clientes da tabela sales.customers
-
--- COMMAND ----------
 
 select email, first_name, last_name
 from sales.customers
@@ -19,29 +15,24 @@ from sales.customers
 -- Seleção todas as colunas de uma tabela 
 --Liste todas as informações dos clientes da tabela sales.customers
 
--- COMMAND ----------
-
 select *
 from sales.customers
 
 -- Seleção de uma coluna sem DISTINCT
 -- Liste as marcas de carro que constam na tabela products
 
--- COMMAND ----------
 select brand
 from sales.products
 
 -- Seleção de uma coluna com DISTINCT
 -- Liste as marcas de carro distintas que constam na tabela products
 
--- COMMAND ----------
 select distinct brand 
 from sales.products
 
 -- Seleção de mais de uma coluna com DISTINCT
 -- Liste as marcas e anos de modelo distintos que constam na tabela products
 
--- COMMAND ----------
 select distinct brand, model_year
 from sales.products
 
@@ -49,7 +40,6 @@ from sales.products
 -- Filtro com condição única
 -- Liste os emails dos clientes da nossa base que moram no estado de Santa Catarina
 
--- COMMAND ----------
 select email, state
 from sales.customers
 where state = 'SC'
@@ -58,7 +48,6 @@ where state = 'SC'
 -- Liste os emails dos clientes da nossa base que moram no estado de Santa Catarina
 -- ou Mato Grosso do Sul
 
--- COMMAND ----------
 select email, state
 from sales.customers
 where state = 'SC' or state = 'MS'
@@ -68,10 +57,46 @@ where state = 'SC' or state = 'MS'
 -- Liste os emails dos clientes da nossa base que moram no estado de Santa Catarina 
 -- ou Mato Grosso do Sul e que tem mais de 30 anos
 
--- COMMAND ----------
 select email, state, birth_date
 from sales.customers
 where state = 'SC' or state = 'MS' and birth_date < '19911228'
+
+-- Ordenação de valores numéricos
+-- Liste produtos da tabela products na ordem crescente com base no preço
+
+select *
+from sales.products
+order by price desc
+
+
+--  Ordenação de texto
+-- Liste os estados distintos da tabela customers na ordem crescente
+
+select distinct state
+from sales.customers
+order by state
+
+
+-- Seleção das N primeiras linhas usando LIMIT
+-- Liste as 10 primeiras linhas da tabela funnel utilizando o LIMIT
+select *
+from sales.funnel
+limit 10
+
+
+-- Seleção das N primeiras linhas usando LIMIT e ORDER BY
+-- Liste os 10 produtos mais caros da tabela products com o comando LIMIT
+select *
+from sales.products
+order by price desc
+limit 10
+  
+
+
+
+
+
+
 
 
 
